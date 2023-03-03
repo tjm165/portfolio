89398f07be694ff7e468762cd6b240ec50bd1627
Thomas M <tjm165@case.edu>
2023-03-03 13:43:15 -0500

    Timeline content [WIP]

--- frontend/src/components/pages/about/index.tsx
+++ frontend/src/components/pages/about/index.tsx
@@ -12,6 +12,7 @@ import LaptopMacIcon from "@mui/icons-material/LaptopMac";
 import HotelIcon from "@mui/icons-material/Hotel";
 import RepeatIcon from "@mui/icons-material/Repeat";
 import Typography from "@mui/material/Typography";
+import { debug } from "console";
 
 const colors = [undefined, "primary", "primary"];
 const variants = ["filled", "filled", "outlined"];
@@ -23,34 +24,51 @@ const getVariant = (i: any, list: any) => {
 
 const timelineItems = [
   {
-    title: "hi",
-    paragraph: "p",
-    date: "9:30 am",
+    title: "Fourth Grade",
+    date: "2009 - 2010",
+    paragraph:
+      "My parents instituted a rule preventing me from playing video games on weeknights. To find a way around this rule, I decided to create my own video game. This required me to learn about coding and web hosting. As my understanding of what goes into a website started to take form, I was able to incrementally build my game. Over time, I turned it into an HTML website where each image clicked would lead to different webpages that represented various game states.",
     icon: <RepeatIcon />,
   },
   {
-    title: "hi",
-    paragraph: "p",
-    date: "9:30 am",
+    title: "Middle School",
+    date: "2011 - 2013",
+    paragraph:
+      "With a growing passion for computer programming I signed up for my school's computer class. However, I was disappointed to find that the class only covered the basics of the Microsoft suite. To pursue my interest, I turned to watching YouTube videos on programming in bash. During my class time, I would secretly create batch files that prompted users for inputs like their name and favorite color, and then responded accordingly. This allowed me to gain knowledge on inputs, conditional statements, loops, and the infamous `goto` command. Several of my classmates took interest in my project. During lunch, I took the opportunity to share my learnings with them.",
     icon: <RepeatIcon />,
   },
   {
-    title: "hi",
-    paragraph: "p",
-    date: "9:30 am",
+    title: "High School",
+    date: "2013 - 2017",
+    paragraph: (
+      <>
+        I was eager to expand my programming skills beyond the limitations of
+        bash, most notably its inability to be hosted on the web. To pursue web
+        programming, I began learning JavaScript, which proved to be quite
+        challenging. Seeking structured guidance, I enrolled in a summer boot
+        camp but made a rookie mistake by signing up for a Java course instead
+        of a JavaScript course. Nonetheless, I was able to learn the basics of
+        Java programming and demonstrated my knowledge by creating a {""}
+        <a href="https://github.com/tjm165/Simon-Game" target="_blank">
+          Simon Says game
+        </a>
+        .
+      </>
+    ),
+
     icon: <RepeatIcon />,
   },
   {
-    title: "hi",
-    paragraph: "p",
-    date: "9:30 am",
-    icon: <RepeatIcon />,
+    title: "College, Case Western Reserve University",
+    date: "2017 - 2021",
+    paragraph: <>Internship at progress book Interneship at McKinsey</>,
   },
   {
-    title: "hi",
-    paragraph: "p",
-    date: "9:30 am",
-    icon: <RepeatIcon />,
+    title: "McKinsey & Company",
+    date: "2021 - Current",
+    paragraph: (
+      <>Engineer 1 on auth Engineer 2 on auth Engineer 2 on no code pro code</>
+    ),
   },
 ];
 
@@ -67,20 +85,22 @@ export default function Contact() {
 export function CustomizedTimeline() {
   return (
     <Timeline position="alternate">
-      {timelineItems.map((val, i) => (
-        <Record
-          key={i}
-          {...val}
-          color={getVariant(i, colors)}
-          variant={getVariant(i, variants)}
-        />
-      ))}
+      {timelineItems
+        .slice()
+        .reverse()
+        .map((val, i) => (
+          <Record
+            key={i}
+            {...val}
+            color={getVariant(i, colors)}
+            variant={getVariant(i, variants)}
+          />
+        ))}
     </Timeline>
   );
 }
 
 function Record({ color, variant, date, title, paragraph, icon }: any) {
-  console.log(color, variant);
   return (
     <TimelineItem>
       <TimelineOppositeContent
