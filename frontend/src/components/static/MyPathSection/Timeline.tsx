4b482c19d77f821a535c7c47fab01412af89970d
Thomas M <tjm165@case.edu>
2023-03-26 22:24:08 -0500

    Reverse

--- frontend/src/components/static/MyPathSection/Timeline.tsx
+++ frontend/src/components/static/MyPathSection/Timeline.tsx
@@ -9,23 +9,23 @@ import timelineItems from "./timelineItemsData";
 function Timeline() {
   return (
     <VerticalTimeline>
-      {timelineItems.map(({ title, date, paragraph, location }, index) => (
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-          date={date}
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          // icon={<WorkIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">{title}</h3>
-          <h4 className="vertical-timeline-element-subtitle">{location}</h4>
-          {/* <p>
-            Creative Direction, User Experience, Visual Design, Project
-            Management, Team Leading
-          </p> */}
-        </VerticalTimelineElement>
-      ))}
+      {timelineItems
+        .reverse()
+        .map(({ title, date, paragraph, location }, index) => (
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            date={location} // yes I intentionally flip flopped this because I think it looks better
+            // icon={<WorkIcon />}
+          >
+            <h3 className="vertical-timeline-element-title">{title}</h3>
+            <h4 className="vertical-timeline-element-subtitle">{date}</h4>
+
+            <p>{paragraph}</p>
+          </VerticalTimelineElement>
+        ))}
     </VerticalTimeline>
   );
 }
