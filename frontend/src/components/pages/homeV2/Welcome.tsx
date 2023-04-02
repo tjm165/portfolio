3f9adc63a770f400b15c24853020f5127d416994
Thomas M <tjm165@case.edu>
2023-04-01 23:21:36 -0500

    Cleanup welcome

--- frontend/src/components/pages/homeV2/Welcome.tsx
+++ frontend/src/components/pages/homeV2/Welcome.tsx
@@ -20,42 +20,40 @@ type PropTypes = {
 };
 
 const HomepageHeading = ({ mobile }: PropTypes) => (
-  <Container text>
-    <Grid container stackable verticalAlign="middle">
-      <Grid.Row>
-        <Grid.Column floated="right" width={8}>
-          <Header
-            as="h1"
-            content="Hi, I'm Thomas Moawad"
-            inverted
-            style={{
-              fontSize: mobile ? "2em" : "4em",
-              fontWeight: "normal",
-              marginBottom: 0,
-              marginTop: mobile ? "1.5em" : "3em",
-            }}
-          />
-          <Header
-            as="h2"
-            content="I'm a full stack software engineer. I love serverless, cloud, and scalability.
+  <Grid container stackable verticalAlign="middle">
+    <Grid.Row>
+      <Grid.Column floated="right" width={8}>
+        <Header
+          as="h1"
+          content="Hi, I'm Thomas Moawad"
+          inverted
+          style={{
+            fontSize: mobile ? "2em" : "4em",
+            fontWeight: "normal",
+            marginBottom: 0,
+            marginTop: mobile ? "1.5em" : "3em",
+          }}
+        />
+        <Header
+          as="h2"
+          content="I'm a full stack software engineer. I love serverless, cloud, and scalability.
       I am frequently recognized for bridging the gap between techical and business concepts."
-            inverted
-            style={{
-              fontSize: mobile ? "1.5em" : "1.7em",
-              fontWeight: "normal",
-              marginTop: mobile ? "0.5em" : "1.5em",
-            }}
-          />
-          <Button primary color="linkedin" size="huge">
-            <Icon name="linkedin" /> Connect on LinkedIn
-          </Button>
-        </Grid.Column>
-        <Grid.Column width={8}>
-          <Image src={require("../../../moawad.jpg")} circular />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </Container>
+          inverted
+          style={{
+            fontSize: mobile ? "1.5em" : "1.7em",
+            fontWeight: "normal",
+            marginTop: mobile ? "0.5em" : "1.5em",
+          }}
+        />
+        <Button primary color="linkedin" size="huge">
+          <Icon name="linkedin" /> Connect on LinkedIn
+        </Button>
+      </Grid.Column>
+      <Grid.Column width={8}>
+        <Image src={require("../../../moawad.jpg")} circular />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
 );
 
 export default HomepageHeading;
