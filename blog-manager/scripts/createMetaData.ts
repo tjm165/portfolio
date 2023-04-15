351f342ade8fa6ab90c21b3e5dc2d6c82cf7fe60
Thomas M <tjm165@case.edu>
2023-04-15 13:03:59 -0400

    cleanup

--- blog-manager/scripts/createMetaData.ts
+++ blog-manager/scripts/createMetaData.ts
@@ -1,40 +1,63 @@
-console.log("Starting meta data");
-
 import * as fs from "fs";
 
 // Path to the directory containing the blog posts
 const postsDirectory = "blog/posts";
 
-// Read the contents of the posts directory
-fs.readdir(postsDirectory, (err, files) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  // Iterate over each file in the directory
-  files.forEach((file) => {
-    // Check if the file is a directory
-    if (fs.statSync(`${postsDirectory}/${file}`).isDirectory()) {
-      // If it is a directory, print the name
-      console.log(file);
-
-      const filePath = `${postsDirectory}/${file}/body.md`;
-
-      // Read the contents of the file
-      fs.readFile(filePath, "utf8", (err, data) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-
-        // Count the number of characters in the file
-        const numCharacters = data.length;
-
-        console.log(
-          `The file at ${filePath} contains ${numCharacters} characters.`
-        );
-      });
+function fileManagement() {
+  const metadata = [];
+  let x = 1;
+  // @ts-ignore
+  fs.readdir(postsDirectory, (err, files) => {
+    metadata.push("blog");
+    x += 1;
+    if (err) {
+      console.log(err);
+      return;
     }
+
+    // Iterate over each file in the directory
+    files.forEach((file) => {
+      metadata.push("1");
+      // Check if the file is a directory
+      if (fs.statSync(`${postsDirectory}/${file}`).isDirectory()) {
+        let blog = {};
+
+        const filePath = `${postsDirectory}/${file}/body.md`;
+
+        // Read the contents of the file
+        fs.readFile(filePath, "utf8", (err, data) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+
+          // Count the number of characters in the file
+          const numCharacters = data.length;
+          blog = {
+            title: "My Title",
+            path: file,
+            numCharacters,
+            description: "Hey this is my first blog!",
+            image: "https://picsum.photos/300/300",
+          };
+          console.log(blog);
+          metadata.push(blog);
+        });
+      }
+    });
   });
-});
+
+  return x;
+}
+
+async function main() {
+  // Read the contents of the posts directory
+
+  console.log("Starting meta data");
+  const m = await fileManagement();
+
+  console.log("***");
+  console.log(m);
+}
+
+main();
