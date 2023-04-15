0e58686e073698c95d76c24f85477b4bc6e0edc8
Thomas M <tjm165@case.edu>
2023-04-15 14:32:32 -0400

    ChatGPT createMetaData

--- blog-manager/scripts/createMetaData.ts
+++ blog-manager/scripts/createMetaData.ts
@@ -1,81 +1,43 @@
-import * as fs from "fs";
+const fs = require("fs");
+const path = require("path");
 
-// Path to the directory containing the blog posts
-const postsDirectory = "blog/posts";
+const basePath = "./blog/posts";
 
-async function fileManagement() {
-  const metadata = [];
-  // @ts-ignore
-  fs.readdir(postsDirectory, async (err, files) => {
-    metadata.push("blog");
-    if (err) {
-      console.log(err);
-      return;
-    }
+fs.readdir(basePath, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-    // Iterate over each file in the directory
-    const promises = files.map((file) => {
-      return new Promise(() => {
-        metadata.push("2");
-        // Check if the file is a directory
-        if (fs.statSync(`${postsDirectory}/${file}`).isDirectory()) {
-          let blog = {};
+  const directories = files
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
 
-          const filePath = `${postsDirectory}/${file}/body.md`;
+  const results = [];
 
-          // Read the contents of the file
-          fs.readFile(filePath, "utf8", (err, data) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
+  directories.forEach((dir) => {
+    const filePath = path.join(basePath, dir, "body.md");
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
 
-            // Count the number of characters in the file
-            const numCharacters = data.length;
-            const blog = {
-              title: "My Title",
-              path: file,
-              numCharacters,
-              description: "Hey this is my first blog!",
-              image: "https://picsum.photos/300/300",
-            };
-            console.log("a", blog);
-            metadata.push(blog);
-            console.log("b", metadata);
+      const numChars = data.length;
+      results.push({ [dir]: numChars });
 
-            fs.writeFile(
-              "example.txt",
-              JSON.stringify(blog) + " \n",
-              { flag: "a" },
-              (err) => {
-                if (err) throw err;
-                console.log("Data written to file");
-              }
-            );
-          });
-        }
-      });
+      if (results.length === directories.length) {
+        const outputFilePath = path.join(basePath, "metadata.json");
+        const jsonData = JSON.stringify(results, null, 2);
+        fs.writeFile(outputFilePath, jsonData, (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+
+          console.log(`Results written to ${outputFilePath}`);
+        });
+      }
     });
-
-    Promise.all(promises).then(() => {
-      console.log("c", metadata);
-    });
-
-    console.log("c", metadata);
   });
-}
-
-async function main() {
-  // Read the contents of the posts directory
-
-  console.log("Starting meta data");
-  fs.writeFile("example.txt", "", (err) => {
-    if (err) throw err;
-    console.log("File cleared");
-  });
-  const m = await fileManagement();
-}
-
-main().then();
-
-// ts-node -e "import main from 'blog-manager/scripts/createMetaData.ts'; (async () => { await main(); })();"
+});
