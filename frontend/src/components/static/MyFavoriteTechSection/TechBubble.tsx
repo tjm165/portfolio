43d0029326ac2fb293d0c1bce6bb68494bb65b0b
Thomas M <tjm165@case.edu>
2023-05-22 21:40:50 -0500

    offset

--- frontend/src/components/static/MyFavoriteTechSection/TechBubble.tsx
+++ frontend/src/components/static/MyFavoriteTechSection/TechBubble.tsx
@@ -3,7 +3,11 @@ import { Card, Image, Popup } from "semantic-ui-react";
 
 const TechBubble = ({ icon, name, description }: TechCardPropTypes) => {
   return (
-    <Popup trigger={<Image src={icon} spaced circular size={"tiny"} />}>
+    <Popup
+      position="left center"
+      hoverable
+      trigger={<Image src={icon} spaced circular size={"tiny"} />}
+    >
       <Popup.Header>{name}</Popup.Header>
       <Popup.Content>{description}</Popup.Content>
     </Popup>
