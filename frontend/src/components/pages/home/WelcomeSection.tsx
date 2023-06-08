65f4175d1b2e9c8dcd0fca14a9dd015de665a81a
Thomas M <tjm165@case.edu>
2023-06-07 23:51:53 -0400

    add breakpoints for headshot

--- frontend/src/components/pages/home/WelcomeSection.tsx
+++ frontend/src/components/pages/home/WelcomeSection.tsx
@@ -1,3 +1,5 @@
+import { createMedia } from "@artsy/fresnel";
+
 import { Button, Divider, Grid, Header, Icon, Image } from "semantic-ui-react";
 import { UndecoratedAnchor } from "../../common";
 import MyFavoriteTechBubbles from "../../static/MyFavoriteTechSection/MyFavoriteTechBubbles";
@@ -5,70 +7,95 @@ import ScrollArrow from "./ScrollArrow";
 
 const mobile = true; // TOMMY SUPPORT BIG TIME
 
+// Tommy put this into my theme
+const { MediaContextProvider, Media } = createMedia({
+  breakpoints: {
+    mobile: 0,
+    tablet: 768,
+    computer: 1024,
+  },
+});
+
 const WelcomeSection = () => {
   return (
-    <div className="Gradient" style={{ minHeight: "90vh" }}>
-      <Grid
-        style={{ minHeight: 700, padding: "1em 0em" }}
-        container
-        stackable
-        verticalAlign="middle"
-      >
-        <Grid.Row>
-          <Grid.Column
-            floated="right"
-            width={7}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Header
-              as="h1"
-              content="Hi, I'm Thomas Moawad"
-              inverted
+    <MediaContextProvider>
+      <div className="Gradient" style={{ minHeight: "90vh" }}>
+        <Grid
+          style={{ minHeight: 700, padding: "1em 0em" }}
+          container
+          stackable
+          verticalAlign="middle"
+        >
+          <Grid.Row>
+            <Grid.Column
+              floated="right"
+              width={7}
               style={{
-                fontSize: mobile ? "2em" : "4em",
-                fontWeight: "normal",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
               }}
-            />
-            <Header
-              as="h2"
-              content="I'm a full stack software engineer. I love serverless, cloud, and scalability.
-      I am frequently recognized for bridging the gap between techical and business concepts."
-              inverted
-              style={{
-                fontSize: mobile ? "1.5em" : "1.7em",
-                fontWeight: "normal",
-                marginTop: mobile ? "0.5em" : "1.5em",
-              }}
-            />
-            <UndecoratedAnchor
-              href="https://www.linkedin.com/in/thomasmoawad/"
-              target="_blank"
             >
-              <Button primary color="linkedin" size="huge">
-                <Icon name="linkedin" /> Connect on LinkedIn
-              </Button>
-            </UndecoratedAnchor>
-            <Divider />
-            <MyFavoriteTechBubbles />
-          </Grid.Column>
-          <Grid.Column
-            width={8}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Image src={require("../../../moawad.png")} circular />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-      <ScrollArrow />
-    </div>
+              <Header
+                as="h1"
+                content="Hi, I'm Thomas Moawad"
+                inverted
+                style={{
+                  fontSize: mobile ? "2em" : "4em",
+                  fontWeight: "normal",
+                }}
+              />
+              <Header
+                as="h2"
+                content="I'm a full stack software engineer. I love serverless, cloud, and scalability.
+      I am frequently recognized for bridging the gap between techical and business concepts."
+                inverted
+                style={{
+                  fontSize: mobile ? "1.5em" : "1.7em",
+                  fontWeight: "normal",
+                  marginTop: mobile ? "0.5em" : "1.5em",
+                }}
+              />
+              <UndecoratedAnchor
+                href="https://www.linkedin.com/in/thomasmoawad/"
+                target="_blank"
+              >
+                <Button primary color="linkedin" size="huge">
+                  <Icon name="linkedin" /> Connect on LinkedIn
+                </Button>
+              </UndecoratedAnchor>
+              <Divider />
+              <Media at="mobile">
+                <Grid.Column
+                  width={8}
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Image src={require("../../../moawad.png")} circular />
+                </Grid.Column>
+              </Media>
+              <MyFavoriteTechBubbles />
+            </Grid.Column>
+            <Grid.Column
+              width={8}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Media greaterThan="mobile">
+                <Image src={require("../../../moawad.png")} circular />
+              </Media>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <ScrollArrow />
+      </div>
+    </MediaContextProvider>
   );
 };
 
