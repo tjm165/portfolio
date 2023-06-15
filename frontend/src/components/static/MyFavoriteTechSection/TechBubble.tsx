0a7b342a850a905434faf93140a024835fb8e1b7
Thomas M <tjm165@case.edu>
2023-06-15 12:03:15 -0500

    fix tech bubble issue

--- frontend/src/components/static/MyFavoriteTechSection/TechBubble.tsx
+++ frontend/src/components/static/MyFavoriteTechSection/TechBubble.tsx
@@ -1,31 +1,32 @@
 import TechCardPropTypes from "./type";
-import { Card, Image, Popup } from "semantic-ui-react";
+import { Image, Popup } from "semantic-ui-react";
 // @ts-ignore
+
 import LazyImage from "react-lazy-blur-image";
 
 const TechBubble = ({ icon, name, description }: TechCardPropTypes) => {
   return (
-    <Popup
-      position="left center"
-      hoverable
-      trigger={
-        <LazyImage
-          uri={icon}
-          // @ts-ignore
-          render={(src) => (
+    <LazyImage
+      uri={icon}
+      // @ts-ignore
+      render={(src) => (
+        <Popup
+          position="left center"
+          hoverable
+          trigger={
             <Image
               src={src}
               style={{ marginTop: "6px", borderRadius: "5px" }}
               spaced
               size={"tiny"}
             />
-          )}
-        />
-      }
-    >
-      <Popup.Header>{name}</Popup.Header>
-      <Popup.Content>{description}</Popup.Content>
-    </Popup>
+          }
+        >
+          <Popup.Header>{name}</Popup.Header>
+          <Popup.Content>{description}</Popup.Content>
+        </Popup>
+      )}
+    />
   );
 };
 
